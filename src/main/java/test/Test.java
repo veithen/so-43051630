1e6f77175d88295890f480f1b902c69a8b228afd
Andreas Veithen <andreas.veithen@gmail.com>
2017-05-21 11:11:08 +0100

    Initial commit

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,68 @@
+package test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        Logger.getLogger("").setLevel(Level.SEVERE);
+        Server server = new Server();
+        
+        server.setThreadPool(new QueuedThreadPool(3));
+        Connector connector = new SelectChannelConnector();
+        connector.setPort(8888);
+        server.setConnectors(new Connector[] { connector });
+        ServletContextHandler handler = new ServletContextHandler(server, "/");
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setContextClass(GenericWebApplicationContext.class);
+        servlet.setContextInitializers(new ApplicationContextInitializer<ConfigurableApplicationContext>() {
+            public void initialize(ConfigurableApplicationContext applicationContext) {
+                XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader((GenericWebApplicationContext)applicationContext);
+                reader.loadBeanDefinitions(new ClassPathResource("server.xml", Test.class));
+            }
+        });
+        ServletHolder servletHolder = new ServletHolder(servlet);
+        servletHolder.setName("spring-ws");
+        servletHolder.setInitOrder(1);
+        handler.addServlet(servletHolder, "/*");
+
+        server.start();
+        
+        StringBuilder buffer = new StringBuilder();
+        for (int i=0; i<90000; i++) {
+            for (int j=0; j<25; j++) {
+                buffer.append("AzU9");
+            }
+            buffer.append((char)10);
+        }
+        ClientUploadRequest request = new ClientUploadRequest();
+        request.setBytes(buffer.toString());
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext(new ClassPathResource("client.xml", Test.class));
+        WebServiceTemplate ws = context.getBean(WebServiceTemplate.class);
+        
+        while (true) {
+            long startTime = System.currentTimeMillis();
+
+            System.out.println(((UploadResponse)ws.marshalSendAndReceive(request)).getBytes().length);
+            
+            System.out.println(System.currentTimeMillis()-startTime);
+        }
+        
+//        server.stop();
+    }
+}
